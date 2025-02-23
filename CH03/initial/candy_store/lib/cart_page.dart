e997f21921af0d33a82a7367dffb0c8f70995ec4
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-02-23 12:59:19 +0200

    3. Let Main & Cart Pages have a reference to CartNotifier & Register the listeners to the (cart) notifier

--- CH03/initial/candy_store/lib/cart_page.dart
+++ CH03/initial/candy_store/lib/cart_page.dart
@@ -1,18 +1,12 @@
-import 'package:candy_store/cart_list_item.dart';
-import 'package:candy_store/cart_list_item_view.dart';
 import 'package:flutter/material.dart';
 
-class CartPage extends StatefulWidget {
-  final ValueNotifier<Map<String, CartListItem>> items;
-  final Function(CartListItem) onRemoveFromCart;
-  final Function(CartListItem) onAddToCart;
+import 'cart_list_item_view.dart';
+import 'cart_notifier.dart';
 
-  const CartPage({
-    super.key,
-    required this.items,
-    required this.onRemoveFromCart,
-    required this.onAddToCart,
-  });
+class CartPage extends StatefulWidget {
+  final CartNotifier cartNotifier;
+
+  const CartPage({super.key, required this.cartNotifier});
 
   @override
   State<CartPage> createState() => _CartPageState();
@@ -20,68 +14,72 @@ class CartPage extends StatefulWidget {
 
 class _CartPageState extends State<CartPage> {
   @override
+  void initState() {
+    super.initState();
+    widget.cartNotifier.addListener(_updateCart);
+  }
+
+  @override
+  void dispose() {
+    //! Since CartPageState did not create cartNotifier and instead received it
+    //! as an argument in the constructor, we only need to remove the listener
+    //! rather than dispose of the entire notifier.
+    widget.cartNotifier.removeListener(_updateCart);
+    super.dispose();
+  }
+
+  void _updateCart() => setState(() {});
+
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Cart')),
-      body: ValueListenableBuilder(
-        valueListenable: widget.items,
-        builder: (context, items, _) {
-          final values = items.values.toList();
-          final totalPrice = values.fold<double>(
-            0,
-            (previous, element) =>
-                previous + (element.product.price * element.quantity),
-          );
-
-          return Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(bottom: 60),
-                child: ListView.builder(
-                  padding: const EdgeInsets.symmetric(vertical: 16),
-                  itemCount: values.length,
-                  itemBuilder: (context, index) => CartListItemView(
-                    item: values[index],
-                    onRemoveFromCart: widget.onRemoveFromCart,
-                    onAddToCart: widget.onAddToCart,
-                  ),
-                ),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 60),
+            child: ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              itemCount: widget.cartNotifier.totalItems,
+              itemBuilder: (context, index) => CartListItemView(
+                item: widget.cartNotifier.items[index],
+                onRemoveFromCart: widget.cartNotifier.removeFromCart,
+                onAddToCart: (item) =>
+                    widget.cartNotifier.addToCart(item.product),
               ),
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  decoration:
-                      BoxDecoration(color: Theme.of(context).primaryColor),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text(
-                        'Total:',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                      Text(
-                        '$totalPrice €',
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
                   ),
-                ),
+                  Text(
+                    '${widget.cartNotifier.totalPrice} €',
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          );
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
