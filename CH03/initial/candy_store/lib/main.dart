28319082c942801fd3e509d1bc3bd371861126c3
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-02-24 13:02:25 +0200

    2. Wrap MaterialApp with CartNotifierProvider to access it (CartNotifier) anywhere in widget tree

--- CH03/initial/candy_store/lib/main.dart
+++ CH03/initial/candy_store/lib/main.dart
@@ -1,15 +1,24 @@
-import 'package:candy_store/main_page.dart';
 import 'package:flutter/material.dart';
+
+import 'cart_notifier.dart';
+import 'cart_notifier_provider.dart';
+import 'main_page.dart';
 
 // At this point, all of the code is in the `lib` folder and we will structure it in Part 3
 void main() {
   runApp(
-    MaterialApp(
-      title: 'Candy store',
-      theme: ThemeData(
-        primarySwatch: Colors.lime,
+    //! Because we will need CartNotifierProvider on almost every page of our app,
+    //! it makes sense to have it at the very root, So we wrap MaterialApp in
+    //! CartNotifierProvider.
+    CartNotifierProvider(
+      cartNotifier: CartNotifier(),
+      child: MaterialApp(
+        title: 'Candy store',
+        theme: ThemeData(
+          primarySwatch: Colors.lime,
+        ),
+        home: const MainPage(),
       ),
-      home: const MainPage(),
     ),
   );
 }
