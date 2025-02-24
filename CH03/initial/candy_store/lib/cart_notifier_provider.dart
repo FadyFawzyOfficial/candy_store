dd268e3d729a096c34107e440baafd3f5ff0e44c
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-02-24 13:03:49 +0200

    1. Improve & Handle if Context does not contain the widget (CartNotifierProvider)

--- CH03/initial/candy_store/lib/cart_notifier_provider.dart
+++ CH03/initial/candy_store/lib/cart_notifier_provider.dart
@@ -11,9 +11,16 @@ class CartNotifierProvider extends InheritedWidget {
   }) : super(child: child);
 
   static CartNotifier of(BuildContext context) {
-    return context
-        .dependOnInheritedWidgetOfExactType<CartNotifierProvider>()!
-        .cartNotifier;
+    final provider =
+        context.dependOnInheritedWidgetOfExactType<CartNotifierProvider>();
+
+    //! Here, we added a null check. If the provider is null, we throw a specific error.
+    //! This way, the user of the widget will at least have an idea of what the problem is.
+    if (provider == null) {
+      throw Exception('No CartNotifierProvider found in context');
+    }
+
+    return provider.cartNotifier;
   }
 
   @override
