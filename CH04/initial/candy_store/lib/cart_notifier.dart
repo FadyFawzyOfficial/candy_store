3403307d3241202e8418399c53245798896d5bbc
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-03-28 15:14:12 +0200

    2. Subscribe to the stream of CartInfo form CartModel in CartNotifier

--- CH04/initial/candy_store/lib/cart_notifier.dart
+++ CH04/initial/candy_store/lib/cart_notifier.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 
 import 'cart_list_item.dart';
+import 'cart_model.dart';
 import 'product_list_item.dart';
 
 class CartNotifier extends ChangeNotifier {
+  final CartModel _cartModel = CartModel();
+
+  CartNotifier() {
+    //! 1. We subscribe to the stream fo CartInfo from CartModel in the constructor of CartNotifier.
+    //! Now, every time there is a change in cartInfoStream, we will update our
+    //! locale fields & notify listeners as before via the notifyListeners() method.
+    _cartModel.cartInfoStream.listen((cartInfo) {
+      _items.clear();
+      _totalItems = cartInfo.totalItems;
+      _totalPrice = cartInfo.totalPrice;
+      cartInfo.items.forEach((key, value) => _items[key] = value);
+      notifyListeners();
+    });
+  }
+
   final Map<String, CartListItem> _items = {};
   double _totalPrice = 0;
   int _totalItems = 0;
@@ -12,44 +28,15 @@ class CartNotifier extends ChangeNotifier {
   double get totalPrice => _totalPrice;
   int get totalItems => _totalItems;
 
-  void addToCart(ProductListItem item) {
-    CartListItem? existingItem = _items[item.id];
-    if (existingItem != null) {
-      existingItem = CartListItem(
-        product: existingItem.product,
-        quantity: existingItem.quantity + 1,
-      );
-      _items[item.id] = existingItem;
-    } else {
-      _items[item.id] = CartListItem(
-        product: item,
-        quantity: 1,
-      );
-    }
+  //! 2. In our addToCart() and removeFromCart() methods, we now have no logic,
+  //! and are just calling the methods of CartModel.
+  void addToCart(ProductListItem item) => _cartModel.addToCart(item);
 
-    _totalItems++;
-    _totalPrice += item.price;
+  void removeFromCart(CartListItem item) => _cartModel.removeFromCart(item);
 
-    notifyListeners();
-  }
-
-  void removeFromCart(CartListItem item) {
-    CartListItem? existingItem = _items[item.product.id];
-    if (existingItem != null) {
-      if (existingItem.quantity > 1) {
-        existingItem = CartListItem(
-          product: existingItem.product,
-          quantity: existingItem.quantity - 1,
-        );
-        _items[item.product.id] = existingItem;
-      } else {
-        _items.remove(item.product.id);
-      }
-    }
-
-    _totalItems--;
-    _totalPrice -= item.product.price;
-
-    notifyListeners();
+  @override
+  void dispose() {
+    _cartModel.dispose();
+    super.dispose();
   }
 }
