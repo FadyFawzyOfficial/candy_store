c77c45099ea58a3003afd3fc1fb8ff1cd6ed84e5
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-03-28 15:14:02 +0200

    1. Implement Stream and StreamController for CartInfo updates

--- CH04/initial/candy_store/lib/cart_model.dart
+++ CH04/initial/candy_store/lib/cart_model.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'cart_info.dart';
 import 'cart_list_item.dart';
 import 'product_list_item.dart';
@@ -10,6 +12,23 @@ class CartModel {
   );
 
   CartInfo get cartInfo => _cartInfo;
+
+  //! 1. we have created Stream and StreamController.
+  //* Here, Stream is what stores the data and StreamController is what helps us
+  //* manage how new data, as well as subscriptions, is emitted.
+  final StreamController<CartInfo> _cartInfoController =
+      StreamController<CartInfo>();
+
+  Stream<CartInfo> get cartInfoStream => _cartInfoController.stream;
+
+  //* We created a Future that returns a single instance of CartInfo.
+  //* This can be used for single reads when we don't need to listen to the updates.
+  Future<CartInfo> get CartInfoFuture async => _cartInfo;
+
+  //! 3. To notify our listener that there will be no more events, we need to call
+  // ! the close() method of StreamController. We encapsulated this in the dispose()
+  //! method of CartModel so that we don't leak the details of the implementation to the outside.
+  void dispose() => _cartInfoController.close();
 
   void addToCart(ProductListItem item) {
     CartListItem? existingItem = _cartInfo.items[item.id];
@@ -28,6 +47,10 @@ class CartModel {
 
     _cartInfo.totalItems++;
     _cartInfo.totalPrice += item.price;
+
+    //! 2. Use the add() method ot StreamController to ad new data to our stream.
+    //? What we passed there is _cartInfo.
+    _cartInfoController.add(_cartInfo);
 
     // notifyListeners();
   }
@@ -48,6 +71,8 @@ class CartModel {
 
     _cartInfo.totalItems--;
     _cartInfo.totalPrice -= item.product.price;
+
+    _cartInfoController.add(_cartInfo);
 
     // notifyListeners();
   }
