30a732d8cb69851d3b20d95f28b88e86a1c072c5
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-03-28 15:29:32 +0200

    2. Use CartState in CartViewModel & modify dependent code

--- CH04/initial/candy_store/lib/cart_view_model.dart
+++ CH04/initial/candy_store/lib/cart_view_model.dart
@@ -2,31 +2,35 @@ import 'package:flutter/material.dart';
 
 import 'cart_list_item.dart';
 import 'cart_model.dart';
+import 'cart_state.dart';
 import 'product_list_item.dart';
 
 class CartViewModel extends ChangeNotifier {
+  // ToDo: Inject this in the DI chapter
   final CartModel _cartModel = CartModel();
+
+  CartState _state = CartState(
+    items: {},
+    totalPrice: 0,
+    totalItems: 0,
+  );
 
   CartViewModel() {
     //! 1. We subscribe to the stream fo CartInfo from CartModel in the constructor of CartNotifier.
     //! Now, every time there is a change in cartInfoStream, we will update our
     //! locale fields & notify listeners as before via the notifyListeners() method.
     _cartModel.cartInfoStream.listen((cartInfo) {
-      _items.clear();
-      _totalItems = cartInfo.totalItems;
-      _totalPrice = cartInfo.totalPrice;
-      cartInfo.items.forEach((key, value) => _items[key] = value);
+      // ToDo: Should actually copy the Map and not just the reference.
+      _state = _state.copyWith(
+        items: cartInfo.items,
+        totalPrice: cartInfo.totalPrice,
+        totalItems: cartInfo.totalItems,
+      );
       notifyListeners();
     });
   }
 
-  final Map<String, CartListItem> _items = {};
-  double _totalPrice = 0;
-  int _totalItems = 0;
-
-  List<CartListItem> get items => _items.values.toList();
-  double get totalPrice => _totalPrice;
-  int get totalItems => _totalItems;
+  CartState get state => _state;
 
   //! 2. In our addToCart() and removeFromCart() methods, we now have no logic,
   //! and are just calling the methods of CartModel.
