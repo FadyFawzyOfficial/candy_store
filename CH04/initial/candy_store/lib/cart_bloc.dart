bd3eeb1b58351a9f7606b7163c1c5c0c103b6942
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-03-30 15:49:00 +0200

    4. Build CartBloc & implement event handlers for loading, adding, removing items & clearing errors

---
+++ CH04/initial/candy_store/lib/cart_bloc.dart
@@ -0,0 +1,94 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'cart_event.dart';
+import 'cart_info.dart';
+import 'cart_model.dart';
+import 'cart_state.dart';
+
+//* 1. Now, instead of extending Cubit, we extend Bloc. Note that in the diamond
+//* brackets, we also specify the parent class of the events that this bloc will handle.
+class CartBloc extends Bloc<CartEvent, CartState> {
+  final CartModel _cartModel = CartModel();
+
+  CartBloc() : super(const CartState(items: {}, totalPrice: 0, totalItems: 0)) {
+    //* 2. In our constructor, we need to provide the on handlers for every type
+    //* of event. This is very important because, as opposed to the cubit,
+    //* we won’t be calling methods on the bloc.
+    //* The on handlers are implemented by the library and they let us control how to handle the events.
+    //! Another feature that comes with these handlers are transformers – while
+    //! we won’t be diving into their details, keep in mind that with transformers,
+    //! you can control all kinds of things: how events are handled
+    //! (sequentially or asynchronously), whether to debounce the events or not, and so on.
+    on<Load>(_onLoad);
+    on<AddItem>(_onAddItem);
+    on<RemoveItem>(_onRemoveItem);
+    on<ClearError>(_onClearError);
+  }
+
+  Future<void> _onLoad(Load event, Emitter emit) async {
+    try {
+      emit(state.copyWith(isProcessing: true));
+      //* 3. When we load our bloc, we will read the initial state of our cart
+      //* from the Model that we created previously.
+      final cartInfo = await _cartModel.cartInfoFuture;
+      // ToDo: Should actually copy the Map and not just the reference, which we
+      // will do at the end ot this chapter
+      emit(
+        state.copyWith(
+          items: cartInfo.items,
+          totalPrice: cartInfo.totalPrice,
+          totalItems: cartInfo.totalItems,
+        ),
+      );
+      emit(state.copyWith(isProcessing: false));
+
+      //! 4. After that, we subscribe to changes from the stream by using a
+      //! special function of the bloc’s Emitter – that is, emit.onEach.
+      //! It handles all of the stream subscriptions and un-subscriptions
+      //! for us and lets us deal with the data that we care about.
+      await emit.onEach(
+        _cartModel.cartInfoStream,
+        onData: (CartInfo cartInfo) => emit(
+          state.copyWith(
+            items: cartInfo.items,
+            totalPrice: cartInfo.totalPrice,
+            totalItems: cartInfo.totalItems,
+          ),
+        ),
+        onError: (Object error, StackTrace stackTrace) =>
+            emit(state.copyWith(error: Exception('Failed to load cart info'))),
+      );
+    } on Exception catch (e) {
+      emit(state.copyWith(error: e));
+    }
+  }
+
+  Future<void> _onAddItem(AddItem event, Emitter emit) async {
+    try {
+      emit(state.copyWith(isProcessing: true));
+      await _cartModel.addToCart(event.item);
+      emit(state.copyWith(isProcessing: false));
+    } on Exception catch (e) {
+      emit(state.copyWith(error: e));
+    }
+  }
+
+  Future<void> _onRemoveItem(RemoveItem event, Emitter emit) async {
+    try {
+      emit(state.copyWith(isProcessing: true));
+      await _cartModel.removeFromCart(event.item);
+      emit(state.copyWith(isProcessing: false));
+    } on Exception catch (e) {
+      emit(state.copyWith(error: e));
+    }
+  }
+
+  Future<void> _onClearError(ClearError event, Emitter emit) async =>
+      emit(state.copyWith(error: null));
+
+  /*  @override
+  Future<void> close() async {
+    _cartModel.dispose();
+    super.close();
+  }*/
+}
