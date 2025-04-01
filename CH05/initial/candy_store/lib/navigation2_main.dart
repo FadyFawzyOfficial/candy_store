9518bac6dc66fe6c3ad73ca9183bb8f0413a9560
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-04-01 13:49:27 +0200

    1. Create the new Navigation2 Candy Store Application (Create navigation2_main.dart file)

---
+++ CH05/initial/candy_store/lib/navigation2_main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const CandyStoreApp());
+
+class CandyStoreApp extends StatefulWidget {
+  const CandyStoreApp({super.key});
+
+  @override
+  State<CandyStoreApp> createState() => _CandyStoreAppState();
+}
+
+class _CandyStoreAppState extends State<CandyStoreApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Candy Store',
+      home: Navigator(
+        pages: const [
+          MaterialPage(
+            child: Scaffold(
+              body: Center(
+                child: Text('Welcome to Candy Store'),
+              ),
+            ),
+          ),
+        ],
+        onPopPage: (route, result) => route.didPop(result),
+      ),
+    );
+  }
+}
