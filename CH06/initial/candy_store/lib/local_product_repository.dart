dbda2b4236f75e0b58a040a20a7ff13ca1f72b3c
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-04-06 14:13:49 +0200

    5. Add methods to LocalProductRepository to cache products fetched from remote & another one to clear the cache

--- CH06/initial/candy_store/lib/local_product_repository.dart
+++ CH06/initial/candy_store/lib/local_product_repository.dart
@@ -1,6 +1,7 @@
-import 'package:candy_store/product.dart';
-import 'package:candy_store/product_repository.dart';
 import 'package:hive/hive.dart';
+
+import 'product.dart';
+import 'product_repository.dart';
 
 class LocalProductRepository implements ProductRepository {
   final Box<Product> _productBox;
@@ -21,4 +22,9 @@ class LocalProductRepository implements ProductRepository {
     // TODO: implement updateProduct
     throw UnimplementedError();
   }
+
+  Future<void> cacheProducts(List<Product> products) async =>
+      await _productBox.addAll(products);
+
+  Future<void> clearProducts() async => await _productBox.clear();
 }
