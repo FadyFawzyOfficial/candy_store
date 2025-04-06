32cb92e4d355b4319ee543d667ca599d1629c522
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-04-06 14:14:19 +0200

    1. Create Products Bloc with its event & state class by Bloc VSCode extension

---
+++ CH06/initial/candy_store/lib/products_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'products_event.dart';
+part 'products_state.dart';
+
+class ProductsBloc extends Bloc<ProductsEvent, ProductsState> {
+  ProductsBloc() : super(ProductsInitial()) {
+    on<ProductsEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
