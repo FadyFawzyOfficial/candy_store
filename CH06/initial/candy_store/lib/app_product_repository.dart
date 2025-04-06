931247adf60ad12bd89ba7de150efe2d5eeb79a1
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-04-06 14:13:54 +0200

    6. Implement fetchProduct method to retrieve products from local or remote data source

--- CH06/initial/candy_store/lib/app_product_repository.dart
+++ CH06/initial/candy_store/lib/app_product_repository.dart
@@ -1,13 +1,38 @@
+import 'local_product_repository.dart';
+import 'network_product_repository.dart';
 import 'product.dart';
 import 'product_repository.dart';
 
+//! In this AppProductRepository class, we inject both the local and API data
+//! sources through its constructor. When the fetchProducts method is called,
+//! it first tries to retrieve candies from the local data source.
+//! If the local data source contains data, it returns that data.
+//! If the local data source is empty, it fetches data from the API data source,
+//! caches it locally, and then returns it.
 class AppProductRepository implements ProductRepository {
+  final NetworkProductRepository _remoteDataSource;
+  final LocalProductRepository _localProductRepository;
+
+  AppProductRepository({
+    required NetworkProductRepository remoteDataSource,
+    required LocalProductRepository localProductRepository,
+  })  : _remoteDataSource = remoteDataSource,
+        _localProductRepository = localProductRepository;
+
   @override
-  Future<List<Product>> fetchProduct() {
-    // Fetch candies from the data source (e.g., local database or API)
-    // Transform the raw data if necessary
-    // Return the list of candies
-    throw UnimplementedError();
+  Future<List<Product>> fetchProduct() async {
+    // Retrieve candies from the local data source
+    final localProducts = await _localProductRepository.fetchProduct();
+
+    // Check if local data source has data
+    if (localProducts.isNotEmpty) {
+      return localProducts;
+    } else {
+      // If local data source is empty, fetch from API and cache it locally
+      final apiProducts = await _remoteDataSource.fetchProduct();
+      await _localProductRepository.cacheProducts(apiProducts);
+      return apiProducts;
+    }
   }
 
   @override
