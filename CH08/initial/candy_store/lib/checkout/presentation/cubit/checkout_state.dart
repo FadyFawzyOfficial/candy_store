be01511872624cf73d0b65544647f6c0f2acbbbe
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-04 00:44:26 +0300

    3. Build CheckoutCubit with its methods and states of Presentation Layer

---
+++ CH08/initial/candy_store/lib/checkout/presentation/cubit/checkout_state.dart
@@ -0,0 +1,24 @@
+part of 'checkout_cubit.dart';
+
+class CheckoutState extends Equatable {
+  final List<String> paymentMethods;
+  final DelayedResult<bool> checkoutResult;
+
+  const CheckoutState({
+    required this.paymentMethods,
+    required this.checkoutResult,
+  });
+
+  CheckoutState copyWith({
+    List<String>? paymentMethods,
+    DelayedResult<bool>? checkoutResult,
+  }) {
+    return CheckoutState(
+      paymentMethods: paymentMethods ?? this.paymentMethods,
+      checkoutResult: checkoutResult ?? this.checkoutResult,
+    );
+  }
+
+  @override
+  List<Object> get props => [paymentMethods, checkoutResult];
+}
