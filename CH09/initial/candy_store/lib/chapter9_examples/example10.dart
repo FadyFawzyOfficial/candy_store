3d551bc39e8631f6cd80860b6c0f1bde12dd1088
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-06 06:38:01 +0300

    10. Add example 10 demonstrating asynchronous operations with exception handling by try catch blocks

---
+++ CH09/initial/candy_store/lib/chapter9_examples/example10.dart
@@ -0,0 +1,28 @@
+void main() async {
+  print('Before');
+
+  try {
+    await printWith1msDelay();
+    await printWith1SecondDelay();
+  } on ImportantException catch (ex, st) {
+    print('An important exception was thrown');
+  } on UnimportantException catch (ex, st) {
+    print('An unimportant exception was thrown');
+  }
+}
+
+Future<void> printWith1msDelay() async {
+  await Future.delayed(const Duration(milliseconds: 1));
+  print('Print with 1ms delay');
+  throw UnimportantException();
+}
+
+Future<void> printWith1SecondDelay() async {
+  await Future.delayed(const Duration(seconds: 1));
+  print('Print with 1 Second delay');
+  throw ImportantException();
+}
+
+class ImportantException implements Exception {}
+
+class UnimportantException implements Exception {}
