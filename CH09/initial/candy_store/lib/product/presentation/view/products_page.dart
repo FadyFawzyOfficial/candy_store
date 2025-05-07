db523130a33c37cededc80f922e1b97ce7e5cb53
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-07 07:40:36 +0300

    6. Refactor ProductsView to include search functionality and improve loading state handling

--- CH09/initial/candy_store/lib/product/presentation/view/products_page.dart
+++ CH09/initial/candy_store/lib/product/presentation/view/products_page.dart
@@ -3,9 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../cart/presentation/bloc/cart_bloc.dart';
 import '../../../cart/presentation/bloc/cart_event.dart';
-import '../widget/product_list_item_view.dart';
 import '../../domain/repository/product_repository.dart';
 import '../bloc/products_bloc.dart';
+import '../widget/product_list_item_view.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
@@ -27,18 +27,43 @@ class ProductsView extends StatelessWidget {
   @override
   Widget build(context) {
     final items = context.select((ProductsBloc bloc) => bloc.state.items);
+    final progress = context
+        .select((ProductsBloc bloc) => bloc.state.loadingResult)
+        .isInProgress;
     return Scaffold(
       appBar: AppBar(title: const Text('Products')),
-      body: ListView.builder(
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index];
-          return ProductListItemView(
-            item: item,
-            onAddToCart: (item) => context.read<CartBloc>().add(AddItem(item)),
-          );
-        },
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search_rounded),
+              ),
+              onChanged: (query) =>
+                  context.read<ProductsBloc>().add(SearchProducts(query)),
+            ),
+          ),
+          const SizedBox(height: 16),
+          if (progress) const CircularProgressIndicator(),
+          if (items.isEmpty && !progress) const Text('No items found'),
+          if (items.isNotEmpty)
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  return ProductListItemView(
+                    item: item,
+                    onAddToCart: (item) =>
+                        context.read<CartBloc>().add(AddItem(item)),
+                  );
+                },
+              ),
+            ),
+        ],
       ),
     );
   }
