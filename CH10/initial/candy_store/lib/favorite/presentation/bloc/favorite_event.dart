3a9d1829696b9db60306913fd4c65f56706b7b64
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-08 08:46:29 +0300

    5. Build Favorite Events

--- CH10/initial/candy_store/lib/favorite/presentation/bloc/favorite_event.dart
+++ CH10/initial/candy_store/lib/favorite/presentation/bloc/favorite_event.dart
@@ -6,3 +6,23 @@ sealed class FavoriteEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class LoadFavorite extends FavoriteEvent {}
+
+class AddFavorite extends FavoriteEvent {
+  final ProductListItem item;
+
+  const AddFavorite({required this.item});
+
+  @override
+  List<Object> get props => [item];
+}
+
+class RemoveFavorite extends FavoriteEvent {
+  final String id;
+
+  const RemoveFavorite({required this.id});
+
+  @override
+  List<Object> get props => [id];
+}
