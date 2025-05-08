01de1163750860b6a27d7795ce9dc28545ce0b59
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-08 08:45:58 +0300

    1. Build FavoriteRepository as interface contains methods definitions

---
+++ CH10/initial/candy_store/lib/favorite/domain/repository/favorite_repository.dart
@@ -0,0 +1,8 @@
+import '../../../product/domain/model/product_list_item.dart';
+
+abstract class FavoriteRepository {
+  Future<List<ProductListItem>> getFavorites();
+  Future<void> addFavorite(ProductListItem item);
+  Future<void> removeFavorite(String id);
+  Future<bool> isFavorite(String id);
+}
