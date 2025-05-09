2a035023f396e31e05023cc777f87c8b97ceeafe
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-09 08:49:29 +0300

    2. Build ProductDetails States

--- CH10/initial/candy_store/lib/product/presentation/bloc/product_details_bloc.dart
+++ CH10/initial/candy_store/lib/product/presentation/bloc/product_details_bloc.dart
@@ -1,13 +1,16 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../domain/model/product_list_item.dart';
 
 part 'product_details_event.dart';
 part 'product_details_state.dart';
 
-class ProductDetailsBloc extends Bloc<ProductDetailsEvent, ProductDetailsState> {
-  ProductDetailsBloc() : super(ProductDetailsInitial()) {
-    on<ProductDetailsEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class ProductDetailsBloc
+    extends Bloc<ProductDetailsEvent, ProductDetailsState> {
+  ProductDetailsBloc({
+    required ProductListItem item,
+  }) : super(ProductDetailsState(item: item, isFavorite: false)) {
+    on<ProductDetailsEvent>((event, emit) {});
   }
 }
--- CH10/initial/candy_store/lib/product/presentation/bloc/product_details_state.dart
+++ CH10/initial/candy_store/lib/product/presentation/bloc/product_details_state.dart
@@ -1,10 +1,21 @@
 part of 'product_details_bloc.dart';
 
-sealed class ProductDetailsState extends Equatable {
-  const ProductDetailsState();
-  
-  @override
-  List<Object> get props => [];
-}
+class ProductDetailsState extends Equatable {
+  final ProductListItem item;
+  final bool isFavorite;
 
-final class ProductDetailsInitial extends ProductDetailsState {}
+  const ProductDetailsState({required this.item, required this.isFavorite});
+
+  ProductDetailsState copyWith({
+    ProductListItem? item,
+    bool? isFavorite,
+  }) {
+    return ProductDetailsState(
+      item: item ?? this.item,
+      isFavorite: isFavorite ?? this.isFavorite,
+    );
+  }
+
+  @override
+  List<Object?> get props => [item, isFavorite];
+}
