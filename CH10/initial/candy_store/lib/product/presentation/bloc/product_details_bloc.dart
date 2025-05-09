e77434eb90b8d4125b3d42e2298c786ffa4ede0c
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-09 08:49:45 +0300

    4. Build ProductDetailsBloc to include FavoriteRepository with favorite management functionality

--- CH10/initial/candy_store/lib/product/presentation/bloc/product_details_bloc.dart
+++ CH10/initial/candy_store/lib/product/presentation/bloc/product_details_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../favorite/domain/repository/favorite_repository.dart';
 import '../../domain/model/product_list_item.dart';
 
 part 'product_details_event.dart';
@@ -8,9 +9,34 @@ part 'product_details_state.dart';
 
 class ProductDetailsBloc
     extends Bloc<ProductDetailsEvent, ProductDetailsState> {
+  final FavoriteRepository _favoriteRepository;
   ProductDetailsBloc({
+    required FavoriteRepository favoriteRepository,
     required ProductListItem item,
-  }) : super(ProductDetailsState(item: item, isFavorite: false)) {
-    on<ProductDetailsEvent>((event, emit) {});
+  })  : _favoriteRepository = favoriteRepository,
+        super(ProductDetailsState(item: item, isFavorite: false)) {
+    on<LoadProductDetails>(_onLoadProductDetails);
+    on<ToggleFavorite>(_onToggleFavorite);
+  }
+
+  Future<void> _onLoadProductDetails(
+    LoadProductDetails event,
+    Emitter<ProductDetailsState> emit,
+  ) async {
+    final isFavorite = await _favoriteRepository.isFavorite(state.item.id);
+    emit(state.copyWith(isFavorite: isFavorite));
+  }
+
+  Future<void> _onToggleFavorite(
+    ToggleFavorite event,
+    Emitter<ProductDetailsState> emit,
+  ) async {
+    final isFavorite = state.isFavorite;
+    emit(state.copyWith(isFavorite: !isFavorite));
+    if (isFavorite) {
+      await _favoriteRepository.removeFavorite(state.item.id);
+    } else {
+      await _favoriteRepository.addFavorite(state.item);
+    }
   }
 }
