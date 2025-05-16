0161c1c38d1376cad291987cadd6a7b74e392247
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-16 06:49:19 +0300

    3. Write the code that would call the native code to add products to favorites (locally)

--- CH10/initial/candy_store/lib/favorite/data/repository/local_favorite_repository.dart
+++ CH10/initial/candy_store/lib/favorite/data/repository/local_favorite_repository.dart
@@ -7,8 +7,12 @@ class LocalFavoriteRepository extends FavoriteRepository {
   static const _platform = MethodChannel('com.example.candy_store/favorite');
 
   @override
-  Future<void> addFavorite(ProductListItem item) {
-    throw UnimplementedError();
+  Future<void> addFavorite(ProductListItem item) async {
+    //! 1. As the first parameter to the invokeMethod method of MethodChannel,
+    //! we pass addFavorite, as string that represents the name of the method
+    //! to be called on the native side.
+    //* 2. To pass parameters, we create a map and add an item.id single value for the 'id' key.
+    _platform.invokeMethod('addFavorite', {'id': item.id});
   }
 
   @override
