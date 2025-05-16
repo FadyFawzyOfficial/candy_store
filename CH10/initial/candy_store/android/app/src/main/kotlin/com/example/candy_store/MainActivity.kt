51b5a53a1a4f40cb04663fc575cf1d9d9232f57d
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-16 06:49:13 +0300

    2. Establish a connection to the MethodChannel on the native side of Android

--- CH10/initial/candy_store/android/app/src/main/kotlin/com/example/candy_store/MainActivity.kt
+++ CH10/initial/candy_store/android/app/src/main/kotlin/com/example/candy_store/MainActivity.kt
@@ -3,4 +3,9 @@ package com.example.candy_store
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FLutterEngine)
+    val favoriteMethodChannel = MethodChannel(
+        flutterEngine.dartExecutor.binaryMessenger,
+        "com.example.candy_store/favorite"
+    )
 }
