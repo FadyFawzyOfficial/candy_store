4fb54ba79edba3fe3ede4d7373e70b2066a8ac41
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-18 07:54:33 +0300

    7. Refactor iOS native code in AppDelegate to implement its native local storage for favorite management, replacing MethodChannel with Pigeon package integration

--- CH10/initial/candy_store/ios/Runner/AppDelegate.swift
+++ CH10/initial/candy_store/ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, LocalStorageApi {
     private let userDefaults = UserDefaults.standard
     
     override func application(
@@ -11,79 +11,38 @@ import Flutter
     ) -> Bool {
         // 1. First, we have obtained the FlutterViewController controller in order to access binaryMessenger.
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-        
-        // 2. Next, we have created an instance of MethodChannel. Once again, the name should completely
-        // match the one defined on the Flutter side.
-        let favoriteChannel = FlutterMethodChannel(
-            name: "com.example.candy_store/favorite",
-            binaryMessenger: controller.binaryMessenger
-        )
-        
-        // 3. After that, we set the method handler on the channel, where we match strings as method names, as before.
-        favoriteChannel.setMethodCallHandler { [weak self] (
-            call: FlutterMethodCall,
-            result: @escaping FlutterResult) in guard let self = self else { return }
-            
-            switch call.method {
-            case "getFavorites":
-                result(self.getFavorites())
-            case "addFavorite":
-                // 4. Similar to Kotlin, we have unwrapped potentially nullable arguments
-                // and implemented error handling for invalid argument cases.
-                //! This means if we can safely cast call.arguments to be a map of type [String: Any],
-                //! and this map contains a String value with the "id" key,
-                //! then execute the following block of code. Otherwise, execute the else code block.
-                if let args = call.arguments as? [String: Any],
-                   let id = args["id"] as? String {
-                    self.toggleFavorite( id, isFavorite: true)
-                    result(nil)
-                } else {
-                    result(FlutterError(
-                        code: "INVALID_ARGUMENT",
-                        message: "id is null",
-                        details: nil)
-                    )
-                }
-            case "isFavorite":
-                if let args = call.arguments as? [String: Any], let id = args["id"] as? String {
-                    result(self.isFavorite(id))
-                } else {
-                    result(FlutterError(
-                        code: "INVALID_VALUE",
-                        message: "id is null",
-                        details: nil)
-                    )
-                }
-            case "removeFavorite":
-                if let args = call.arguments as? [String: Any], let id = args["id"] as? String {
-                    self.toggleFavorite(id, isFavorite: false)
-                    result(nil)
-                } else {
-                    result(FlutterError(code: "INVALID_VALUE", message: "id is null", details: nil))
-                }
-            default:
-                result(FlutterMethodNotImplemented)
-                // 5. Finally, we have returned a result, which can be nil or values
-                // (such as in getFavorites) indicating success, FlutterError in case of an error,
-                // or FlutterMethodNotImplemented if we don't know how to handle the method.
-                result(FlutterMethodNotImplemented)
-            }
-        }
-        
+
+        LocalStorageApiSetup.setUp(binaryMessenger: controller.binaryMessenger, api: self)
         
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
     
-    private func getFavorites() -> [String] {
-        if let favorites = userDefaults.array(forKey: "favorites") as? [String]{
-            return favorites;
-        }
-        return []
+    func addFavorite(id: String) {
+        toggleFavorite(id, isFavorite: true)
     }
     
-    private func isFavorite(_ id: String) -> Bool {
-        return getFavorites().contains(id)
+    func getFavorites() -> [FavoriteProduct] {
+        let favoriteIds = getFavoriteIds()
+        let favorites = favoriteIds.map { id in 
+            return FavoriteProduct(id: id)
+        }
+        return favorites
+    }
+
+    func isFavorite(id: String) -> Bool {
+        return getFavoriteIds().contains(id)
+    }
+
+    func removeFavorite(id: String) {
+        toggleFavorite(id, isFavorite: false)
+    }
+    
+    private func getFavoriteIds() -> [String] {
+        if let favorites = userDefaults.array(forKey: "favorites") as? [String] {
+            return favorites
+        }
+        return []
     }
     
     private func toggleFavorite(_ id: String, isFavorite: Bool) {
@@ -91,7 +50,7 @@ import Flutter
         if isFavorite {
             currentFavorites.append(id)
         } else {
-            currentFavorites.removeAll { $0 == id}
+            currentFavorites.removeAll { $0 == id }
         }
         
         userDefaults.set(currentFavorites, forKey: "favorites")
