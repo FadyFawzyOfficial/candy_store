79bee8e78b2d91abb68f55c3b7d5301e7a41059e
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-24 10:22:43 +0300

    6. Enhance CartBloc unit tests to verify item removal functionality with Mockito

--- CH11/initial/candy_store/test/mockito_example/mockito_unit_test.dart
+++ CH11/initial/candy_store/test/mockito_example/mockito_unit_test.dart
@@ -2,11 +2,13 @@ import 'dart:async';
 
 import 'package:candy_store/cart/cart.dart';
 import 'package:candy_store/cart/domain/model/cart_info.dart';
+import 'package:candy_store/cart/domain/model/cart_list_item.dart';
 import 'package:candy_store/cart/presentation/bloc/cart_state.dart';
 import 'package:candy_store/common/model/delayed_result.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
+import '../test_data.dart';
 import 'cart_repository.mocks.dart';
 
 void main() {
@@ -51,5 +53,73 @@ void main() {
         ),
       ),
     );
+
+    //* This test setup and execution ensures that CartBloc correctly handles
+    //* the removal of an item from the cart. By using mockito to mock CartRepository,
+    //* the test isolates the behavior of CartBloc and verifies its interactions
+    //* with the repository, ensuring that the bloc emits the correct states throughout the process.
+    test('Remove item form cart', () async {
+      // We can begin the test setup for removing an item from the cart.
+      // It initializes the product and cartItem, which represents a product and
+      // its instance in the cart. It also sets up initialCartInfo to represent the
+      // cart's state before the removal and emptyCartInfo to represent the cart's
+      // state after the removal.
+      final product = TestData.testProductListItem;
+      final cartItem = CartListItem(product: product, quantity: 1);
+      final initialCartInfo = CartInfo(
+        items: {product.id: cartItem},
+        totalPrice: product.price.toDouble(),
+        totalItems: 1,
+      );
+      final emptyCartInfo = CartInfo(
+        items: {},
+        totalPrice: 0,
+        totalItems: 0,
+      );
+
+      //! Now, we must set up mock behavior for CartRepository. Initially,
+      //! cartInfoFuture returns the initialCartInfo. When removeFromCart is called,
+      //! it simulates a network delay, then updates the cartInfoFuture so that it
+      //! returns emptyCartInfo and adds emptyCartInfo to the cartInfoController stream:
+      when(mockCartRepository.cartInfoFuture)
+          .thenAnswer((_) async => initialCartInfo);
+      when(mockCartRepository.removeFromCart(cartItem)).thenAnswer((_) async {
+        // simulate network delay
+        await Future.delayed(const Duration(milliseconds: 50));
+        when(mockCartRepository.cartInfoFuture)
+            .thenAnswer((_) async => emptyCartInfo);
+        cartInfoController.add(emptyCartInfo);
+      });
+
+      //! We must pre-populate the cart with the product to set up the initial
+      //! state for the test. This involves calling addToCart ont he repository
+      //! and dispatching a Load event to CartBloc:
+      await mockCartRepository.addToCart(product);
+      cartBloc.add(const Load());
+      await Future.delayed(const Duration(milliseconds: 200));
+
+      //* We can also add a list to collect state changes and start listening to
+      //* the CartBloc stream, adding each state to the list as it changes:
+      final states = <CartState>[];
+      final subscription = cartBloc.stream.listen(states.add);
+
+      //! With this snippet, we're dispatching a RemoveItem event to CartBloc,
+      //! and waiting for the event to be processed. After a delay, cancel the
+      //! subscription to stop listening to state changes:
+      cartBloc.add(RemoveItem(cartItem));
+      await Future.delayed(const Duration(seconds: 1));
+      await subscription.cancel();
+
+      //! Verify state changes
+      //! As a result, we check every step, ensure there are at least three states,
+      //! check the loading states, and verify that the final state reflects that
+      //! the item has been removed form the cart.
+      // Should have at least 3 states
+      expect(states.length, greaterThanOrEqualTo(3));
+      expect(states[0].loadingResult.isInProgress, isTrue);
+      expect(states[1].loadingResult.isInProgress, isFalse);
+      expect(states[2].items.length, equals(0));
+      expect(states[2].totalPrice, equals(0));
+    });
   });
 }
