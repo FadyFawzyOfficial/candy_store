e6ea439e84b7f5628fadd6c2f6b5c2a6cd63457c
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-05-25 12:26:33 +0300

    2. Add custom lint rule into analysis_options.yaml file to force use double quote for Strings

--- CH12/initial/candy_store/lib/main.dart
+++ CH12/initial/candy_store/lib/main.dart
@@ -1,19 +1,19 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import "package:flutter/material.dart";
+import "package:flutter_bloc/flutter_bloc.dart";
 
-import 'cart/data/repository/in_memory_cart_repository.dart';
-import 'cart/domain/repository/cart_repository.dart';
-import 'cart/presentation/view/cart_page.dart';
-import 'favorite/data/api/local_storage_api.g.dart';
-import 'favorite/data/repository/local_favorite_repository.dart';
-import 'favorite/domain/repository/favorite_repository.dart';
-import 'main_page.dart';
-import 'product/data/repository/app_product_repository.dart';
-import 'product/data/repository/local_product_repository.dart';
-import 'product/data/repository/network_product_repository.dart';
-import 'product/data/service/api_service.dart';
-import 'product/data/service/hive_service.dart';
-import 'product/domain/repository/product_repository.dart';
+import "cart/data/repository/in_memory_cart_repository.dart";
+import "cart/domain/repository/cart_repository.dart";
+import "cart/presentation/view/cart_page.dart";
+import "favorite/data/api/local_storage_api.g.dart";
+import "favorite/data/repository/local_favorite_repository.dart";
+import "favorite/domain/repository/favorite_repository.dart";
+import "main_page.dart";
+import "product/data/repository/app_product_repository.dart";
+import "product/data/repository/local_product_repository.dart";
+import "product/data/repository/network_product_repository.dart";
+import "product/data/service/api_service.dart";
+import "product/data/service/hive_service.dart";
+import "product/domain/repository/product_repository.dart";
 
 // At this point, all of the code is in the `lib` folder and we will structure it in Part 3
 Future<void> main() async {
@@ -43,12 +43,12 @@ Future<void> main() async {
       child: MaterialApp(
         // On web the "favorite" icon is hidden due to banner, this is done for convenience
         debugShowCheckedModeBanner: false,
-        title: 'Candy Store',
+        title: "Candy Store",
         theme: ThemeData(primarySwatch: Colors.lime),
-        initialRoute: '/',
+        initialRoute: "/",
         routes: {
-          '/': (context) => MainPage.witBloc(),
-          '/cart': (context) => CartPage.withBloc(),
+          "/": (context) => MainPage.witBloc(),
+          "/cart": (context) => CartPage.withBloc(),
         },
       ),
     ),
